2ada77b48385567ba1e2d8bf1882365cce8d2390
alejandro <alejandro@civir.es>
2023-01-24 15:54:42 +0000

    Segundo

--- main.tf
+++ main.tf
@@ -2,19 +2,30 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_instance" "Terralejandro" {
+resource "aws_instance" "terralejandro" {
     ami = "ami-00874d747dde814fa"
     instance_type = "t2.micro"
 
     tags = {
         Name = "Terralejandro"
     }
-    /*network_interface {
-      network_interface_id = aws_network_interface.Terralejandro
-      device_index = 0
-    }
-    
-    credit_specification {
-      cpu_credits = "unlimited"
-    }*/
+
+    user_data = <<-EOF
+      #!/bin/bash
+      echo "Hello, World!" > index.html
+      nohup busybox httpd -f -p 8080 &
+      EOF
+
+    user_data_replace_on_change = true
+
+    vpc_security_group_ids = [aws_security_group.allow_web.id]
+}
+
+resource "aws_security_group" "allow_web" {
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
